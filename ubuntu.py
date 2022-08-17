0f153b797410fbf2b71afcfebf79a040b9908185
Dylan Wondal <djsimpson22@outlook.com.au>
2022-08-17 14:33:21 +1000

    added check users and fixed unauth files

--- ubuntu.py
+++ ubuntu.py
@@ -18,29 +18,35 @@ def update():
 # check if firewall is installed and if not install it and turn it on
 def ufw_check():
     print("Checking if the firewall is enabled")
-    if os.popen("sudo ufw status").read() == 'Status: inactive':
-        os.popen("sudo ufw enable > /dev/null")
+    if os.system("sudo ufw status").read() == 'Status: inactive':
+        os.system("sudo ufw enable > /dev/null")
         print("Firewall started")
     else:
         print("Firewall already active")
         
-def unauthorised_files(ext):
-    print("Removing unautorised files")
-    for i in ext:
-        os.popen("sudo find / -name *." + i)
+def unauthorised_files():
+    print("Removing unauthorised files")
+    disallowedExt = input("What are the disallowed file extensions? (seperate by comma e.g. mp3,mp4)\nfile extension: ") # get user input for files not allowed according to readme from the box
+    notallowedExt = []
+    notallowedExt = disallowedExt.split(",") # split the input by comma and add it to the array
+    for i in notallowedExt:
+        print("The disallowed files with the extension " + i + " are: \n")
+        os.system("sudo find / -name *." + i + " 2>/dev/null") # find files with extension and send errors to null
         
     
+def checkUsers():
+    print("Listing all users, please check readme to see which users are not allowed\n")
+    os.system("sudo getent passwd |cut -d: -f1")
     
+
 
 # Main Function
 def main():
     check_root()
     update()
     ufw_check()
-    disallowedExt = input("What are the disallowed file extensions? (seperate by comma e.g. mp3,mp4)\nfile extension: ")
-    notallowedExt = []
-    notallowedExt = disallowedExt.split(",")
-    unauthorised_files(notallowedExt)
+    unauthorised_files()
+    checkUsers()
     
 
 
